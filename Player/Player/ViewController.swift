5407c2984e854c48041dbe4262b60647eb15e92d
iTSangar <itsangardev@gmail.com>
2015-11-04 18:37:31 -0200

    slider

--- Player/Player/ViewController.swift
+++ Player/Player/ViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Skuby: UISlider {
+    override func trackRectForBounds(bounds: CGRect) -> CGRect {
+        var result = super.trackRectForBounds(bounds)
+        result.origin.x = -1
+        result.size.height = 4
+        result.size.width = bounds.size.width + 2
+        return result
+    }
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var skub: Skuby!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        skub.setThumbImage(UIImage(named: "skubidu")!, forState: .Normal)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
 }
 
